121cf54388cab10a7067e15a3e06efd34bbaeff7
SY97P <54990890+SY97P@users.noreply.github.com>
2021-09-09 23:39:01 +0900

    [프언어]

---
+++ 4주차/hw1/hw1/hw1.mli
@@ -0,0 +1,10 @@
+type day = SUN | MON | TUE | WED | THU | FRI | SAT
+type grade = Aplus | A | Bplus | B | Cplus | C | Dplus | D | F
+
+val student_number: int
+val email: string
+val phone_number: string
+val favorite_day: day
+val expected_grade: grade
+val is_male: bool
+val result: float 
